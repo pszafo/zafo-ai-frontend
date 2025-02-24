cc873671cac5fd4ae7f5ba1515b98f11c06a6c55
pszafo <ps@zafo.in>
2025-02-24 21:51:12 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EventTracker from "./components/EventTracker";
+
+function App() {
+  return (
+    <div>
+      <h1>Zafo AI - User Event Tracker</h1>
+      <EventTracker />
+    </div>
+  );
+}
+
+export default App;
